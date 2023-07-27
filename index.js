4af945c828766714057e8f23f15bdf38c85be8a0
Haroon <haroon.ali.1104@gmail.com>
2023-07-27 13:07:16 +0100

    get commits from week ago

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { google } = require("googleapis");
+const moment = require("moment");
 
 const creds = require("./creds.json");
 
@@ -11,16 +12,29 @@ async function updateGitHub() {
         const authClientObject = await auth.getClient();
         const googleSheetsInstance = google.sheets({ version: "v4", auth: authClientObject });
         const spreadsheetId = "14_qFhVEdgBLXScjCHFySI6NFZMnT3l02bKDoGhxq4ZM";
-    
+
         const readData = await googleSheetsInstance.spreadsheets.values.get({
             auth,
             spreadsheetId,
             range: "GitHub!C2",
         })
-    
+
         const username = readData.data.values[0][0];
-    
-        console.log(username);
+
+        const today = moment().format("YYYY-MM-DD");
+        const weekAgo = moment().subtract(8, 'days').format("YYYY-MM-DD");
+        console.log(weekAgo);
+
+        const res = await fetch(`https://api.github.com/search/commits?q=author:${username}+committer-date:>${weekAgo}`, {
+            headers: {
+                'Authorization': `Bearer ${creds.gitHubToken}`,
+                'Accept': 'application/vnd.github+json',
+                'X-GitHub-Api-Version': '2022-11-28'
+            }
+        });
+        const data = await res.json();
+
+        console.log(data["total_count"]);
     } catch (error) {
         console.log(error.message);
     }
