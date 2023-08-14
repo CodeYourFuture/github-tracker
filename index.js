08d40240674a9ca5fa15402111fd379aa658a479
Haroon <haroon.ali.1104@gmail.com>
2023-08-14 13:39:29 +0100

    remove console log

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const googleSheets = require("./googleSheets");
 
 async function fillSheet() {
     const users = await googleSheets.getUsers();
-    console.log(users);
 }
 
 module.exports.fillSheet = fillSheet;
